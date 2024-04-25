233f5627e6c4ceaf1d20de28fa56e21cec8b3aff
belgiusti <isabelaag1@al.insper.edu.br>
2024-04-25 20:26:58 -0300

    COMMIT1

---
+++ TRANSPOTES_E_PAISES.py
@@ -0,0 +1,62 @@
+TRANSPORTE = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
+
+# frotas de cada pais
+PAISES =  { 
+    1:{
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }},
+      2:{
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    }},
+    3:{
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    }},
+    4:{
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    }},
+    5:{
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }}
+}
+
+print (PAISES)
+
+numero_nacao = input('Qual o numeor da nação da sua frota?')
+
+for numero in PAISES:
+    if numero == numero_nacao:
+        for pais, tranporte_nacao in numero.items():
+            for transporte_nome, quantidade in tranporte_nacao.items():
+
