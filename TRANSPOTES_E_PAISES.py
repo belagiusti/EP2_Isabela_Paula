96285fa83e9eef748264b63673e75aef4eba63b1
belgiusti <isabelaag1@al.insper.edu.br>
2024-04-29 19:36:34 -0300

    Transporte do código

--- TRANSPOTES_E_PAISES.py
+++ TRANSPOTES_E_PAISES.py
@@ -1,97 +1,106 @@
-##Cria matriz quadrada de espaços
-def cria_mapa(N):
-    mapa = [[' ' for _ in range(N)] for _ in range(N)]
-    return mapa
 
+TRANSPORTE = {
+    'destroyer': 3,
+    'porta-avioes': 5,
+    'submarino': 2,
+    'torpedeiro': 3,
+    'cruzador': 2,
+    'couracado': 4
+}
 
+# frotas de cada pais
+PAISES =  { 
+    1:{
+    'Brasil': {
+        'cruzador': 1,
+        'torpedeiro': 2,
+        'destroyer': 1,
+        'couracado': 1,
+        'porta-avioes': 1
+    }},
+      2:{
+    'França': {
+        'cruzador': 3, 
+        'porta-avioes': 1, 
+        'destroyer': 1, 
+        'submarino': 1, 
+        'couracado': 1
+    }},
+    3:{
+    'Austrália': {
+        'couracado': 1,
+        'cruzador': 3, 
+        'submarino': 1,
+        'porta-avioes': 1, 
+        'torpedeiro': 1
+    }},
+    4:{
+    'Rússia': {
+        'cruzador': 1,
+        'porta-avioes': 1,
+        'couracado': 2,
+        'destroyer': 1,
+        'submarino': 1
+    }},
+    5:{
+    'Japão': {
+        'torpedeiro': 2,
+        'cruzador': 1,
+        'destroyer': 2,
+        'couracado': 1,
+        'submarino': 1
+    }}
+}
 
+print (PAISES)
 
+lista_transporte_nome = []
+numero_nacao = input('Qual o número da nação da sua frota?')
 
-#Navio pode ser alocado na posição?
-def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
-    # Verifica se a posição inicial está dentro do mapa
-    if linha < 0 or coluna < 0 or linha >= len(mapa) or coluna >= len(mapa[0]):
-        return False
+for numero in PAISES:
+    if numero == numero_nacao:
+        for pais, transportes_pais_qnt in numero.items():
+            print ('Você escolheu a nação {0}'. format(pais))
+            print ('Agora é a sua vez de alocar seus navios de guerra!')
+#            print (TABULEIRO)
 
-    # Verifica se a orientação é válida
-    if orientacao not in ['v', 'h']:
-        return False
+            for transporte_nome, quantidade in transportes_pais_qnt.items():
+                lista_transporte_nome.append(transporte_nome)
+                for transporte_TRANSPORTE, qnt_blocos in TRANSPORTE.items():
+                    if transporte_nome == transporte_TRANSPORTE:
+                        blocos = qnt_blocos 
+                        print ('alocar: {0} ({1} blocos)'.format(transporte_nome, blocos))
+                        for i in range(len(lista_transporte_nome)):
+                            if transporte_nome != lista_transporte_nome[0]:
+                                del lista_transporte_nome [i - 1]
+                                resto_transporte = (' ').join(lista_transporte_nome)
+                            else:   
+                                resto_transporte = (' ').join(lista_transporte_nome)
+                            if resto_transporte != '':
+                                print ('proximos:{0}'.format(resto_transporte))
+                                informe_letra = input('Informe a Letra:')
+                                informe_linha = input('Informe a Linha:')
+                                informe_orientacao = input ('Informe a Orientação [v|h]:')
+                                print ('Navio alocado!')
+                            if resto_transporte == '':
+                                informe_letra = input('Informe a Letra:')
+                                informe_linha = input('Informe a Linha:')
+                                informe_orientacao = input ('Informe a Orientação [v|h]:')
+                                print ('Todos os navios foram alocados!')
+                            #PRINT TABULEIRO 
 
-    # Verifica se há conflitos com outros navios ou se o navio sai dos limites do mapa
-    if orientacao == 'v':
-        if linha + blocos > len(mapa):
-            return False
-        for i in range(linha, linha + blocos):
-            if mapa[i][coluna] != ' ':
-                return False
-    else:
-        if coluna + blocos > len(mapa[0]):
-            return False
-        for j in range(coluna, coluna + blocos):
-            if mapa[linha][j] != ' ':
-                return False
+# INICIANDO BATALHA NAVAL 
 
-    return True
+print ('Iniciando a batalha naval!')
+# print ('5')
+# time.sleep(1)
+# print ('4')
+# time.sleep(1)
+# print ('3')
+# time.sleep(1)
+# print ('2')
+# time.sleep(1)
+# print ('1')
 
-
-
-
-#Alocando navios para o computador
-
-import random
-
-def aloca_navios(mapa, blocos):
-    def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
-        # Verifica se a posição inicial está dentro do mapa
-        if linha < 0 or coluna < 0 or linha >= len(mapa) or coluna >= len(mapa[0]):
-            return False
-
-        # Verifica se a orientação é válida
-        if orientacao not in ['v', 'h']:
-            return False
-
-        # Verifica se há conflitos com outros navios ou se o navio sai dos limites do mapa
-        if orientacao == 'v':
-            if linha + blocos > len(mapa):
-                return False
-            for i in range(linha, linha + blocos):
-                if mapa[i][coluna] != ' ':
-                    return False
-        else:
-            if coluna + blocos > len(mapa[0]):
-                return False
-            for j in range(coluna, coluna + blocos):
-                if mapa[linha][j] != ' ':
-                    return False
-
-        return True
-
-    n = len(mapa)
-
-    for navio_blocos in blocos:
-        alocado = False
-        while not alocado:
-            linha = random.randint(0, n-1)
-            coluna = random.randint(0, n-1)
-            orientacao = random.choice(['h', 'v'])
-
-            if posicao_suporta(mapa, navio_blocos, linha, coluna, orientacao):
-                if orientacao == 'v':
-                    for i in range(linha, linha + navio_blocos):
-                        mapa[i][coluna] = 'N'
-                else:
-                    for j in range(coluna, coluna + navio_blocos):
-                        mapa[linha][j] = 'N'
-                alocado = True
-
-    return mapa
-
-
-
-
-#Verifica se acabou os 'N's da matriz
-def foi_derrotado(matriz):
-    for linha in matriz:
-        if 'N' in linha:
-            return False
-    return True
+              
+#               PRINT (TABULEIRO) 
