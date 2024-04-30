d7ce8aaf3263b0ca387eaba68cfabc6225219e45
paulaehrenberger <paulafe@al.insper.edu.br>
2024-04-30 08:34:16 -0300

    Tab Comp

---
+++ Tabuleiro_Comp.py
@@ -0,0 +1,22 @@
+dic_mapa_comp= {
+    'letra': ['A','B','C','D','E','F','G','H','I','J'],
+    '1': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','1'],
+    '2': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','2'],
+    '3': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','3'],
+    '4': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','4'],
+    '5': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','5'],
+    '6': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','6'],
+    '7': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','7'],
+    '8': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','8'],
+    '9': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','9'],
+    '10': [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ','10'],
+    'letra2': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+}
+
+print("   " + "  ".join(dic_mapa_comp['letra']))  # Imprime a linha de letras
+
+for i in range(1, 11):
+    linha = dic_mapa_comp[str(i)]  # Pega a linha correspondente do dicionário
+    print(str(i).rjust(2) + " " + "  ".join(linha))  # Imprime a linha com o número à esquerda e os valores separados por espaço
+print("   " + "  ".join(dic_mapa_comp['letra2']))  # Imprime a linha de letras
+
